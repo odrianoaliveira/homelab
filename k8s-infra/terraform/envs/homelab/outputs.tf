671359a755bd35e1db6e0132cc637fea11291e38
Adriano de Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2025-12-23 16:54:54 +0100

    refactor project structure

---
+++ k8s-infra/terraform/envs/homelab/outputs.tf
@@ -0,0 +1,22 @@
+
+output "talos_configs" {
+  description = "Talos machine configurations"
+  value       = module.cluster.talosconfig
+  sensitive   = true
+}
+
+output "kubeconfig" {
+  description = "Kubeconfig for the cluster"
+  value       = module.cluster.kubeconfig
+  sensitive   = true
+}
+
+output "cluster_endpoint" {
+  description = "Kubernetes cluster endpoint"
+  value       = module.cluster.endpoint
+}
+
+output "vm_nodes" {
+  description = "VM nodes"
+  value       = module.infra.nodes
+}
---
+++ k8s-infra/terraform/envs/homelab/variables.tf
@@ -0,0 +1,21 @@
+variable "proxmox_api_url" {
+  type        = string
+  description = "Proxmox API URL, e.g., https://<host>:8006/api2/json"
+}
+
+variable "proxmox_token_id" {
+  type        = string
+  description = "Proxmox API token ID in the format user@realm!tokenname"
+}
+
+variable "proxmox_token_secret" {
+  type        = string
+  description = "Proxmox API token secret value."
+  sensitive   = true
+}
+
+variable "proxmox_tls_insecure" {
+  type        = bool
+  description = "Allow insecure TLS (self-signed certs). Set to false in production."
+  default     = true
+}
